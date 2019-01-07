4509c85f500710365899f57e184e4b42ea0ea904
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-01-07 15:51:18 -0600

    Finished Syntax, Types, and Variables exercise

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,15 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
         int myFavoriteNumber = 509;
-        String myString = 'j';
+        String myString = "Justin";
         System.out.println(myString);
+        long myNumber = 38787914L;
+        System.out.println(myNumber);
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+        double pi = 3.14159;
+        int three = Integer.parseInt("3");
+        System.out.println(three);
     }
 }
