596184c6f70e9e713105b10d6d6f35cbca0d04a4
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-01-07 12:24:00 -0600

    Added Basics.java file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
         int myFavoriteNumber = 509;
-        String myString = 'j';
+        String myString = "Justin";
         System.out.println(myString);
     }
 }
