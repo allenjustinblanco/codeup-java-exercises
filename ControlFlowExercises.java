22d1407f3513341cd4b8eb37d0574ea8e4c9af27
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-01-09 10:14:31 -0600

    Added work for Strings java exercise

--- ControlFlowExercises.java
+++ ControlFlowExercises.java
@@ -10,18 +10,16 @@ public class ControlFlowExercises {
       i++;
     }
 
-    int x = 2;
-    do {
-      System.out.println(x);
-      x=x*x;
-    } while (x <= 10000);
+    // long x = 2;
+    // do {
+    //   System.out.println(x);
+    //   x=x*x;
+    // } while (x <= 10000);
 
     for(int x=2; x<=10000; x*=x){
       System.out.println(x);
     }
-    for(x<1000000; x*x){
-      System.out.println(x);
-    }
+
     int n = 100;
     for(int z=1; z<n; z++){
       System.out.println(z);
@@ -34,7 +32,15 @@ public class ControlFlowExercises {
       }
     }
     System.out.println("Enter an Integer");
-    int userEntry = scan.next();
-    
+    String userEntry = scan.next();
+    System.out.println("Would you like to continue? Y/N");
+    boolean userInput = scan.next().equalsIgnoreCase("yes") || scan.next().equalsIgnoreCase("y");
+    // for(int i=0; i<=userEntry; i++){
+    //   int intArray = intArray + i;
+    // };
+    String userTable = "number: ";
+    if(userInput){
+      System.out.println(userTable);
+    }
   }
 }
