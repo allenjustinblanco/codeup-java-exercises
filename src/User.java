d6ae8cd8b2449078fb069e6714a260557276993d
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-01-14 15:38:20 -0600

    Added work for User bonus exercise

---
+++ src/User.java
@@ -0,0 +1,11 @@
+
+
+public class User{
+  private username;
+  private password;
+  private numberOfSuccessfulLogins;
+  private numberOfFailedLogins;
+  private boolean isLoggedIn = false;
+  private static int numberOfUsers;
+  public login(username, password){}
+}
