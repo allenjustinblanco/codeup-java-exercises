665b064e5fbeb6283e367bb6d8041159b19c7fa1
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-01-14 15:58:21 -0600

    Added methods for User login

--- src/User.java
+++ src/User.java
@@ -1,11 +1,31 @@
-
+import java.util.Scanner;
 
 public class User{
-  private username;
-  private password;
-  private numberOfSuccessfulLogins;
-  private numberOfFailedLogins;
+  Scanner scan = new Scanner(System.in).useDelimitter("\n");
+
+  private String username(){
+    System.out.println("Enter username?");
+    String username = scan.next()
+    return username;
+  }
+  private String password(){
+    System.out.println("Enter password?");
+    String password = scan.next();
+    return password;
+  }
+  private int numberOfSuccessfulLogins(){
+
+  }
+  private int numberOfFailedLogins;
   private boolean isLoggedIn = false;
   private static int numberOfUsers;
-  public login(username, password){}
+  public login(username, password){
+    if((this.username.equals(username)) && this.password.equals(password)){
+
+    }
+  }
+  public logout(){
+
+  }
+  }
 }
