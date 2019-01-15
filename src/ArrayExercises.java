061465dd00dc3ad66f20e7cfd98a5f1bdf0ddae0
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-01-15 11:35:53 -0600

    Added work for addPerson ArrayExercises

---
+++ src/ArrayExercises.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class ArrayExercises{
+  public static void main(String[] args){
+    int[] numbers = {1, 2, 3, 4, 5};
+    System.out.println(Arrays.toString(numbers));
+
+    Person[] Person = new Person[4];
+
+    Person[0] = new Person("Bob");
+    Person[1] = new Person("Joe");
+    Person[2] = new Person("Bill");
+    Person[3] = new Person("Sam");
+
+    for(Person x : Person){
+      System.out.println(x);
+    }
+  }
+  public static void addPerson(Person[] people){
+    Person[] people = new Person[Person[].length() + 1];
+  }
+}
