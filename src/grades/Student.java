4f34c6e94ac01496358ab082471519772e6cf226
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-01-18 10:49:25 -0600

    Added work for Array List exercise

---
+++ src/grades/Student.java
@@ -0,0 +1,44 @@
+package grades;
+import java.util.ArrayList;
+
+public class Student{
+  private String name;
+  private static ArrayList<Integer> grades = new ArrayList<>();
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  // returns the student's name
+  public String getName(){
+    return name;
+  }
+
+  // adds the given grade to the grades property
+  public void addGrade(int grade){
+    grades.add(grade);
+  }
+
+  // returns the average of the students grades
+  //add each grade together then divide them by grades length
+  double averageGrade;
+  public double getGradeAverage(){
+    int length = grades.size();
+    for(int i = 0; i < grades.size(); i++){
+      averageGrade += grades.get(i);
+    }
+    return averageGrade/length;
+  }
+
+  public static void main(String[] args){
+    Student student1 = new Student();
+    student1.addGrade(50);
+    student1.addGrade(50);
+    student1.addGrade(100);
+    student1.addGrade(100);
+
+    System.out.println(student1.getGradeAverage());
+    
+  }
+
+}
